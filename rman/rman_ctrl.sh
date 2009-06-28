80d43f083eb181adf89623901f6a642b2079b830
Marcus Vinicius Ferreira <marcus.ferreira@abril.com.br>
2009-06-28 11:07:46 -0300

    Using _format

--- rman/rman_ctrl.sh
+++ rman/rman_ctrl.sh
@@ -6,10 +6,13 @@
 # 2009/Jun
 #
 
-source /u01/app/oracle/config/env-ora-10.2.0.sh
+# set: ORACLE_HOME
+#      ORACLE_SID
+#      PATH,CONFIG,MAIL_TO
+source /u01/app/oracle/config/env-ora.sh
 
 DIR=/mnt/rman
-DST=${DIR}/$( /bin/date '+%Y-%m-%d' )
+DST=${DIR}/$( date '+%Y-%m-%d' )
 LOG=${ORACLE_BASE}/log/rman_full.log
 
 [ ! -d $DST ] && mkdir -p $DST
@@ -17,25 +20,27 @@ LOG=${ORACLE_BASE}/log/rman_full.log
 exec 1>>$LOG
 exec 2>>$LOG
 
-time rman $( cat /u01/app/oracle/config/rman_target_${ORACLE_SID}.txt ) <<RMAN
+_format="${DST}/%d_%I_%Y-%M-%D_set-%s-%p-%c"
+
+time rman $( cat $CONFIG/rman_target_${ORACLE_SID}.txt ) <<RMAN
 
 run {
     # %d: db name
-    # %I: dbid: diferencia entre clones do mesmo db
+    # %I: dbid: a different id between clones of a same db
     # %s-%p-%c: "set-piece-copynumber"
 
     sql 'alter system archive log current';
 
-    allocate channel t2 type disk format '${DST}/%d_%I_%Y-%M-%D_set-%s-%p-%c_ctrl.rman' maxpiecesize 4G ;
+    allocate channel t2 type disk format '${_format}_ctrl.rman' maxpiecesize 4G ;
     backup 
         tag 'CONTROLFILE' current controlfile;
     release channel t2;
 
     sql 'alter system archive log current';
-
 }
 
 exit
 
 RMAN
+
 
