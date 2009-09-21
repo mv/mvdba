9d17e3577e0014c673b156f5692a6fb3b7fd81fe
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2009-09-21 12:53:51 -0300

    Added.

---
+++ sql/tab-trunc.sql
@@ -0,0 +1,34 @@
+--
+--
+-- tab-trunc.sql
+--    trunc from user tables
+--
+-- Usage:
+--     SQL> @tab-del
+--
+--    Marcus Vinicius Ferreira                  ferreira.mv[ at ] gmail.com
+--    2009-08
+--
+
+
+SET PAGESIZE 200
+SET FEEDBACK OFF
+SET VERIFY   OFF
+
+SET HEADING OFF
+SET TIMING  OFF
+
+SELECT 'truncate table '|| RPAD(table_name, 31, ' ') || ';'
+  FROM user_tables
+ WHERE 1=1
+   AND 1=1
+ ORDER BY table_name
+/
+
+SET HEADING ON
+SET TIMING  ON
+
+SET FEEDBACK ON
+SET VERIFY   ON
+
+
