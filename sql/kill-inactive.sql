dd5f48a94f2ec6324395615f1ba2c8cdcea25fb9
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2009-11-18 11:21:28 -0200

    Added.

---
+++ sql/kill-inactive.sql
@@ -0,0 +1,25 @@
+--
+-- Kill inactive sessions
+--
+-- 2009/11
+
+set time off
+set timing off
+set feedback off
+set pagesize 0
+set echo off
+
+spool /tmp/1.sql
+
+select 'alter system kill session '||CHR(39)||sid||','
+                                            ||serial#||CHR(39)||';'
+  from v$session
+ where username = NVL('&1',username)
+   and status   = 'INACTIVE'
+/
+
+spool off
+
+set echo on
+@/tmp/1.sql
+
