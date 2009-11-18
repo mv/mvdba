9537dbfda706a5ee53c0070b53a024fa0b06bb74
Marcus Vinicius Ferreira <ferreira.mv@gmail.com>
2009-11-18 12:21:50 -0200

    Moving to ksh: coprocess pipe and only one db session

--- oratop/oratop.sh
+++ oratop/oratop.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ksh
 #
 #
 
@@ -16,27 +16,43 @@ then connect="${DIR}/${CONN}"
 else connect="$1"
 fi
 
+###
+### one session only: to read and write
+###     |& is a ksh feature
+sqlplus -s  $( cat $connect ) |&
+
+print -p "set feedback off"
+print -p "set serveroutput on size 1000000"
+print -p "set linesize 200"
+print -p "set time off"
+print -p "set timing off"
+print -p "exec dbms_application_info.set_module( 'monit', 'oratop');"
+
 while true
 do
-    clear
+    # ksh: force integer for arithmetic
+    typeset -i vsize
+    typeset -i hsize
     which stty 1>/dev/null && vsize=`stty size|cut -f1 -d" "` || vsize=24
     which stty 1>/dev/null && hsize=`stty size|cut -f2 -d" "` || hsize=80
 
+    # send to session
+    print -p "exec system.monit.oratop( $hsize, $vsize );"
 
-    sqlplus -s $( cat $connect ) <<SQL
+    # display adjusts
+    let vsize=$vsize-4 # offset of bottom messages
+    clear
 
-        set feedback off
-        set serveroutput on size 1000000
-        set linesize 200
-        set time off
-        set timing off
-        exec dbms_application_info.set_module( 'monit', 'oratop');
-        exec system.monit.oratop( $hsize, $vsize );
+    # read from session
+    for i in {1..$vsize}
+    do
+        read -p c
+        echo "$c"
+    done
 
-SQL
-    echo
-    echo "Press CTRL-c to exit"
-    sleep 5
+    # bottom messages
+    echo "Press CTRL-c to exit" # - v: $vsize, h: $hsize"
+    sleep 2
 
 done
 
